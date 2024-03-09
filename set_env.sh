bae9854809cc8858521f1cad2292625525a946e6
rajashekar a reddy <rraerrabotu@gmail.com>
2024-03-09 15:48:44 -0600

    all

--- set_env.sh
+++ set_env.sh
@@ -1,15 +1,15 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=XXXXX
+export POSTGRES_PASSWORD=Anvesh123
 export POSTGRES_HOST=udagram-ms-project-db.cqaeh2bjj4cv.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=udacity-udagram-application-images
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export APIUSER_PORT=8081
-export APIFEED_PORT=8082
+export APIUSER_PORT=8080
+export APIFEED_PORT=8080
 export FRONTEND_PORT=8100
 export NODE_OPTIONS=--openssl-legacy-provider
 export URL="http://localhost:8100"
