6cab41ab04ba9ed62996f4a48a2c3b43b70fc177
rajashekar a reddy <rraerrabotu@gmail.com>
2024-03-19 16:20:49 -0500

    ext ip

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=XXXXXX
+export POSTGRES_PASSWORD=Anvesh123
 export POSTGRES_HOST=udagram-ms-project-db-1.cstyeuhjxn6n.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=udacity-udagram-application-images-2
